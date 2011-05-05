6c269b734fc96a7a4e8d56eba5c08afd48985848
unknown <sebastian.schmeck@.saxsys.de>
2011-05-05 14:27:13 +0200

    Klasse Cashier um Klassenbeschreibung erweitert

--- src/main/java/de/saxsys/dojo/cinema/Cashier.java
+++ src/main/java/de/saxsys/dojo/cinema/Cashier.java
@@ -1,5 +1,14 @@
 package de.saxsys.dojo.cinema;
 
+/**
+ * Die Klasse Cashier bildet eine Kinokasse ab, an der man der folgende
+ * Reihenfolge bezahlt:
+ * <ol>
+ * <li>Film auswählen, {@link #startPurchase(String title)}</li>
+ * <li>Alle Tickets mit Altersangabe wählen, {@link #addTicket(int age)}</li>
+ * <li>Preis berechnen lassen, {@link #finishPurchase()}</li>
+ * </ol>
+ */
 public class Cashier {
 
 	private int totalPrice;
